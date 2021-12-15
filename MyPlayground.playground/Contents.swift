66f8388214d9bb4704e8e42039dcc6807b999e79
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-15 14:37:12 +0300

    For loop

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,23 +1,14 @@
 import UIKit
 
-
-var myNum = 32
-
-if myNum == 33{
-    print(myNum)
-} else if myNum > 33{
-    print("old man")
-} else {
-    print("Young man!")
-}
-
-
-switch myNum {
-case 33:
-    print(myNum)
-case 0..<33:
-    print("Young men!")
-default:
-    print("old")
+for  i in 0..<10{
+    print(i)
     
 }
+
+var numbers : [Int] = [4,76,2,6,1]
+
+for n in numbers{
+    print(n)
+}
+
+
