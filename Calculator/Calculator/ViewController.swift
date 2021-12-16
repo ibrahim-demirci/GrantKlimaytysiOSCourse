41d9d20c66dcbe4567fe125eb970f56357116f2f
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-17 00:25:01 +0300

    numbersClicked, operationsClicked and equalsClicked outlets added. And check buttons with tags

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // Button actions
+    @IBAction func numberClicked(_ sender: UIButton){
+        
+    }
+    @IBAction func operatorClicked(_ sender: UIButton){
+        switch sender.tag {
+        case 10:
+            print("add")
+        case 11:
+            print("minus")
+        case 12:
+            print("multiply")
+        case 13:
+            print("divide")
+        default:
+            return
+        }
+        
+    }
+    @IBAction func equalsClicked(_ sender: UIButton){
+        
     }
 
 
