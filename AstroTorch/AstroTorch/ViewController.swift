9e284ac6d8c6c7bf29b11dd7b82cbe374fcd779f
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-16 06:35:40 +0300

    Astro torch project created and View backround changed as Red

---
+++ AstroTorch/AstroTorch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstroTorch
+//
+//  Created by İbrahim Demirci on 16.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
