98b4765f064ad1493405fa1a650163adb0c8de14
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-16 17:34:27 +0300

    Main screen items connected to code using outlets

--- TempratureConverter/TempratureConverter/ViewController.swift
+++ TempratureConverter/TempratureConverter/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tempEntry: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertClicked(_ sender: Any) {
+        print("convert clicked")
+    }
+    
 }
 
