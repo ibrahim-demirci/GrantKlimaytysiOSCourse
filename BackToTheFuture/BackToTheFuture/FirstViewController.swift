219a84f030a6b7d8c6cfe8f1a4b8cebc372774e8
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-18 17:17:22 +0300

    Graphical element added on first tab screen and anchored. All of these connected to code

--- BackToTheFuture/BackToTheFuture/FirstViewController.swift
+++ BackToTheFuture/BackToTheFuture/FirstViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var label1: UIView!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
