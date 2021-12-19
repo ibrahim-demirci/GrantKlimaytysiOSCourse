e0e451dd362620ecc2cc43c03cf68b0739aed9a4
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-20 02:35:31 +0300

    Create VisitedPoint class

---
+++ WhereWasI/WhereWasI/DataModel.swift
@@ -0,0 +1,19 @@
+//
+//  DataModel.swift
+//  WhereWasI
+//
+//  Created by İbrahim Demirci on 20.12.2021.
+//
+
+import Foundation
+
+class VisitedPoint {
+    
+    var latitude: String
+    var longitude: String
+    
+    init(latitude: String, longitude: String) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
