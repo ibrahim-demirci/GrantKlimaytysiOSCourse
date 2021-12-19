e1029c7b9326974981ee22f98929c476033b2610
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-20 00:52:15 +0300

    Reques user locations and show on map

--- WhereWasI/WhereWasI/ViewController.swift
+++ WhereWasI/WhereWasI/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    let locationManager = CLLocationManager()
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else {
+            print("Location not enabled")
+            return
+        }
+        print("Location allowed")
+        mapView.showsUserLocation = true
     }
 
 
