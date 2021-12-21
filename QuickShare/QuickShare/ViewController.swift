4cf04ff92c45bca2dd4afb0aabecc721a76ca6cd
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-21 15:43:29 +0300

    Setup table view with dummy data

--- QuickShare/QuickShare/ViewController.swift
+++ QuickShare/QuickShare/ViewController.swift
@@ -7,15 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    let reuseIdentifier = "tableViewCell"
+    var dummyObjects = ["hi","there","i'm","awesome"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = dummyObjects[indexPath.row]
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyObjects.count
+    }
+    
 
 }
 
