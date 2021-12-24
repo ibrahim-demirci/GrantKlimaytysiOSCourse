4502148aa0d7816c0f0844abd5069f512ce107c1
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-24 22:45:54 +0300

    Design Login Screen

---
+++ ChatAppSwift/ChatAppSwift/LoginRegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginRegisterViewController.swift
+//  ChatAppSwift
+//
+//  Created by İbrahim Demirci on 24.12.2021.
+//
+
+import UIKit
+
+class LoginRegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginClicked(_ sender: Any) {
+    }
+    
+    @IBAction func registerClicked(_ sender: Any) {
+    }
+    
+    @IBAction func forgotClicked(_ sender: Any) {
+    }
+}
