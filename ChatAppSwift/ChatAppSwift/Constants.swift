7577cbb1bc9bc85fa50de091c121678d3d11dcb7
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-25 05:14:31 +0300

    Send and receive data firebase

---
+++ ChatAppSwift/ChatAppSwift/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ChatAppSwift
+//
+//  Created by İbrahim Demirci on 25.12.2021.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    struct MessageFields {
+        static let dateTime = "dateTime"
+        static let text = "text"
+        
+    }
+}
--- ChatAppSwift/ChatAppSwift/ViewController.swift
+++ ChatAppSwift/ChatAppSwift/ViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     
     
     var messages: [DataSnapshot]! = [DataSnapshot]()
+    var ref: DatabaseReference!
+    private var _refHandle: DatabaseHandle!
+    
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
     override func viewWillAppear(_ animated: Bool) {
@@ -34,12 +39,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    deinit {
+        self.ref.child("messages").removeObserver(withHandle: _refHandle)
+    }
+    
+    func ConfigureDatabase() {
+        ref = Database.database().reference()
+        
+        _refHandle = self.ref.child("messages").observe(.childAdded,with:           {(snapshot) -> Void in
+            self.messages.append(snapshot)
+            self.tableView.insertRows(at: [IndexPath(row:self.messages.count-1,section: 0),], with: UITableView.RowAnimation.automatic)
+        })
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell",for: indexPath)
         let messageSnap: DataSnapshot = self.messages[indexPath.row]
         let message = messageSnap.value as! Dictionary<String,String>
-        let text = message["text"] as! String
-        cell.textLabel?.text = text
+        if let text = message[Constants.MessageFields.text] as? String {
+            cell.textLabel?.text = text
+        }
+        
         return cell
         
     }
@@ -59,12 +79,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         self.textField.delegate = self
         
+        ConfigureDatabase()
+        
         
         // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
           
               // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
             NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func SendMessage(data: [String: String]) {
+        let packet = data
+        self.ref.child("messages").childByAutoId().setValue(packet)
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
@@ -83,12 +110,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       self.view.frame.origin.y = 0
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let data = [Constants.MessageFields.text: textField.text! as String]
+        SendMessage(data: data)
         print("ended editing")
         self.view.endEditing(false)
         return true
     }
-    
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
